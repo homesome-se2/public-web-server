61f9c7b40163a770734a3aa1f24f9179c80f5448
Vincenzo <vincebshock@gmail.com>
2020-10-20 07:51:31 +0200

    🎨 pane-room-selection added

---
+++ src/components/pane/pane-room-selection/PaneRoomSelection.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class PaneRoomSelection extends Component {
+    state = {  }
+    render() { 
+        return ( null );
+    }
+}
+ 
+export default PaneRoomSelectinullon;
