24688f7ec3c798f9ed4be2c73a0d780f323f9ca7
Vincenzo <vincebshock@gmail.com>
2020-10-20 07:45:43 +0200

    🎨 control-pager added

--- src/components/controls/controls-1ubutton/Controls1ubutton.js
+++ src/components/controls/controls-1ubutton/Controls1ubutton.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Controls1ubutton extends Component {
     state = {  }
     render() { 
-        return (  );
+        return ( null );
     }
 }
  
---
+++ src/components/controls/controls-pager/ControlsPager.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class ControlsPager extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default ControlsPager;
