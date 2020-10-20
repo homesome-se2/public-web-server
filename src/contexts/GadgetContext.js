a0bc6b0c68bd02f3aa635659ef03c544c5e3c667
Vincenzo <vincebshock@gmail.com>
2020-10-20 07:54:43 +0200

    🎨 GadgetContext added

---
+++ src/contexts/GadgetContext.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { createContext } from 'vm';
+
+export const GadgetContext = createContext();
+
+class GadgetContextProvider extends Component {
+    state = {  }
+    render() { 
+        return ( null );
+    }
+}
+ 
+export default GadgetContext;
