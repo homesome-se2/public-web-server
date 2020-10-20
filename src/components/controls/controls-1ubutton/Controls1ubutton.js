50f85f9a9f1cec7fc3c37c69a96ed879e136fb06
Vincenzo <vincebshock@gmail.com>
2020-10-20 07:42:24 +0200

    🎨 controls-* added

---
+++ src/components/controls/controls-1ubutton/Controls1ubutton.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Controls1ubutton extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Controls1ubutton;
