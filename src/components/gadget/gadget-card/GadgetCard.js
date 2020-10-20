a8ddc24c1022f5f29feb2c8cb12cbce59d05ff6d
Vincenzo <vincebshock@gmail.com>
2020-10-20 07:47:41 +0200

    🎨 gadget-card added

---
+++ src/components/gadget/gadget-card/GadgetCard.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class GadgetCard extends Component {
+    state = {  }
+    render() { 
+        return ( null );
+    }
+}
+ 
+export default GadgetCard;
