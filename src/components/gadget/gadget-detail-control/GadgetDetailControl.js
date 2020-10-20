b52d42d0f3eaf4105a5fdd3201735f39d0bdbb4c
Vincenzo <vincebshock@gmail.com>
2020-10-20 07:48:26 +0200

    🎨 gadget-detail-control added

---
+++ src/components/gadget/gadget-detail-control/GadgetDetailControl.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class GadgetDetailControl extends Component {
+    state = {  }
+    render() { 
+        return ( null );
+    }
+}
+ 
+export default GadgetDetailControl;
