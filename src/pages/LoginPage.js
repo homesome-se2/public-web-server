2af0caeb95485f0017a3c609215cd633b012eb10
Vincenzo <vincebshock@gmail.com>
2020-10-22 12:41:27 +0200

    LoginPage added

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+class LoginPage extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            null
+         );
+    }
+}
+ 
+export default LoginPage;
