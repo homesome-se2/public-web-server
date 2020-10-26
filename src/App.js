07dec3fb010ca49225c2724aa403d8dd44de1776
Vincenzo <vincebshock@gmail.com>
2020-10-26 09:15:31 +0100

    BrowserRouter && ProtectedRoutes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import ProtectedRoute from "./routes/ProtectedRoutes";
+import LoginPage from "./pages/auth/login/LoginPage";
+import DashboardPage from "./pages/dashboard/DashboardPage";
 
 function App() {
   return (
     <div className="hs-webjs-app">
-      <h1>Good Fellows</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <ProtectedRoute exact={true} path="/" component={DashboardPage} />
+          <ProtectedRoute component={DashboardPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
