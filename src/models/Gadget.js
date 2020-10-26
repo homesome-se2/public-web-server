984dd7395db98e58be269757c62807919d960ec3
Vincenzo <vincebshock@gmail.com>
2020-10-26 16:03:32 +0100

    Gadget model class added

---
+++ src/models/Gadget.js
@@ -0,0 +1,13 @@
+class Gadget {
+  constructor(id, alias, type, valueTemplate, state, pollDelaySec){
+      this.id = id;
+      this.alias = alias;
+      this.type = type;
+      this.valueTemplate = valueTemplate;
+      this.state = state;
+      this.pollDelaySec = pollDelaySec;
+  }
+
+}
+ 
+export default Gadget;
