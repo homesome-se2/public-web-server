92d385506b16acd0e53b989227a2dad2e45bf28a
Vincenzo <vincebshock@gmail.com>
2020-10-26 19:00:33 +0100

    UserContext setState methods added

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -9,11 +9,29 @@ class UserContextProvider extends Component {
         isAdmin: '',
         homeAlias: '',
         token: '',
+        gadgets: [],
         csInstance: new ConnectionService('ws://134.209.198.123:8084/homesome')
      }
+     updateGadgets = (gadgets) => {
+        this.setState({gadgets: gadgets})
+     }
+     setUsername = (username) => {
+        this.setState({username: username})
+     }
+     setAdminFlag = (adminFlag) => {
+        this.setState({isAdmin: adminFlag})
+     }
+     setHomeAlias = (homeAlias) => {
+        this.setState({homeAlias: homeAlias})
+     }
+     setToken = (token) => {
+        this.setState({token: token})
+     }
+   
     render() { 
         return ( 
-            <UserContext.Provider value={ {...this.state} }>
+            <UserContext.Provider value={ {...this.state, updateGadgets: this.updateGadgets, setUsername: this.setUsername, 
+            setAdminFlag: this.setAdminFlag, setHomeAlias: this.setHomeAlias, setToken: this.setToken} }>
                 {this.props.children}
             </UserContext.Provider>
          );
