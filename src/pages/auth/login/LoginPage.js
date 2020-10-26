8278ec4c4f5407bad297878d4396d31b54c26937
Vincenzo <vincebshock@gmail.com>
2020-10-26 10:26:00 +0100

    LoginPage user-context provider added

--- src/pages/auth/login/LoginPage.js
+++ src/pages/auth/login/LoginPage.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import LoginForm from '../../../components/form/LoginForm';
 import { ReactComponent as HSLogo } from '../../../assets/images/hs-webjs-logo.svg';
 import './LoginPage.css';
+import UserContextProvider from '../../../contexts/UserContext';
 
 
 
@@ -9,14 +10,16 @@ class LoginPage extends Component {
     state = {  }
     render() { 
         return ( 
+            <UserContextProvider>
             <div className="login-page">
                 <div className="fw-wrapper center">
                     <HSLogo className="header-logo center" />
                     <h1 className="black-medium">Sign in to your <b>smart-home</b> dashboard</h1>
                     <LoginForm />
                 </div>
-                
+               
             </div>
+            </UserContextProvider>
          );
     }
 }
