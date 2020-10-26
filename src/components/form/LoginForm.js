2fc3099256472559129b12b5a888af50d5516f04
Vincenzo <vincebshock@gmail.com>
2020-10-26 15:21:27 +0100

    LoginForm login, ProtectedRoute and sec added

--- src/components/form/LoginForm.js
+++ src/components/form/LoginForm.js
@@ -1,48 +1,100 @@
 import React, { Component } from "react";
+import { withRouter } from 'react-router-dom';
 import './LoginForm.css';
 
-import { TextField, Button } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 import ControlsHeroButton from "../controls/controls-hero-button/ControlsHeroButton";
+import { UserContext } from "../../contexts/UserContext";
+import LSTokenService from "../../services/LSTokenService";
+
 
 
 class LoginForm extends Component {
+
+  static contextType = UserContext;
+
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      username: '',
+      password: '',
+      attemptedLogin: 0,
+      invalid: false
+    };
 
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
+  componentDidMount() {
+    
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  navigate = (target, param) => {
+    this.props.history.push({
+        pathname: `/${target}`,
+        state: param
+    });
+}
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({attemptedLogin: ++this.state.attemptedLogin});
+
+    if(this.areFieldsValid()){
+      this.context.csInstance.connect();
+      this.context.csInstance.login(this.state.username, this.state.password).then((rData) => {
+        console.log(rData)
+        this.context.username = this.state.username;
+        this.context.isAdmin = rData.isAdmin;
+        this.context.homeAlias = rData.homeAlias;
+        this.context.token = rData.token;
+        LSTokenService.setAdminFlag(!!rData.isAdmin);
+        LSTokenService.setHomeAlias(rData.homeAlias);
+        LSTokenService.setToken(rData.token);
+
+        this.navigate("dashboard", {});
+
+      }).catch((rData) => {
+        console.log(rData);
+        this.setState({invalid: true});
+      });
+    }
+    
   }
 
-  handleClick = (e) => {
-    this.handleSubmit(e);
+  areFieldsValid = () => {
+    return this.state.username.length >= 3 && this.state.password.length >=3;
   }
+
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
        
         <TextField className="input-field" id="filled-basic" label="Username" 
-        value={this.state.value} 
-        onChange={this.handleChange} required fullWidth/>
+        onChange={ e => {this.setState({username: e.target.value})} } required fullWidth
+        error={ this.state.invalid || (this.state.username.length <= 3 && this.state.attemptedLogin>0 )}
+        
+        />
 
         <TextField className="input-field"  
         id="filled-basic" label="Password" 
-        type="password" required fullWidth/>
+        type="password" required fullWidth
+          onChange={ e => {this.setState({password: e.target.value})} }
+          error={ this.state.invalid || (this.state.username.length <= 3 && this.state.attemptedLogin>0)}
+        />
 
-
-        <ControlsHeroButton onButtonClick={this.handleClick} label="Login"></ControlsHeroButton>
+        {this.state.invalid &&
+          <span className="login-error">
+            <h2>*Incorrect username or password!</h2>
+          </span>
+          
+        }
+        <ControlsHeroButton onButtonClick={this.handleSubmit} label="Login"></ControlsHeroButton>
       </form>
     );
   }
 }
 
-export default LoginForm;
+export default withRouter(LoginForm);
