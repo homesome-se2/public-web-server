f780f1bcb04c956c10e2befe1c897a4db75f94ad
Vincenzo <vincebshock@gmail.com>
2020-10-26 15:20:51 +0100

    UserContext added default state

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,11 +1,15 @@
 import React, {Component, createContext} from 'react';
+import ConnectionService from '../services/ConnectionService';
 
 export const UserContext = createContext();
 
 class UserContextProvider extends Component {
     state = { 
-        isAuth: false,
-        token: null
+        username: '',
+        isAdmin: '',
+        homeAlias: '',
+        token: '',
+        csInstance: new ConnectionService('ws://134.209.198.123:8084/homesome')
      }
     render() { 
         return ( 
