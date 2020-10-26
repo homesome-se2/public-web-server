9a3f9ef32089c808cff052639a81b3802f0e6a02
Vincenzo <vincebshock@gmail.com>
2020-10-26 19:00:10 +0100

    Multiple to single context transition

--- src/pages/auth/login/LoginPage.js
+++ src/pages/auth/login/LoginPage.js
@@ -1,28 +1,23 @@
-import React, {Component} from 'react';
-import LoginForm from '../../../components/form/LoginForm';
-import { ReactComponent as HSLogo } from '../../../assets/images/hs-webjs-logo.svg';
-import './LoginPage.css';
-import UserContextProvider from '../../../contexts/UserContext';
-
-
+import React, { Component } from "react";
+import LoginForm from "../../../components/form/LoginForm";
+import { ReactComponent as HSLogo } from "../../../assets/images/hs-webjs-logo.svg";
+import "./LoginPage.css";
 
 class LoginPage extends Component {
-    state = {  
-
-    }
-    render() { 
-        return ( 
-            <UserContextProvider>
-            <div className="login-page">
-                <div className="fw-wrapper center">
-                    <HSLogo className="header-logo center" />
-                    <h1 className="black-medium">Sign in to your <b>smart-home</b> dashboard</h1>
-                    <LoginForm />
-                </div>
-            </div>
-            </UserContextProvider>
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <div className="login-page">
+        <div className="fw-wrapper center">
+          <HSLogo className="header-logo center" />
+          <h1 className="black-medium">
+            Sign in to your <b>smart-home</b> dashboard
+          </h1>
+          <LoginForm />
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default LoginPage;
