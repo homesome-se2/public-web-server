6ae6cd89aa710e5ae9bedc7baa10f6789c55b1da
Vincenzo <vincebshock@gmail.com>
2020-10-26 11:37:33 +0100

    ReceivingCommandCodes added

--- src/components/helpers/HoSoHelper.js
+++ src/components/helpers/HoSoHelper.js
@@ -1,29 +1,35 @@
 class HoSoHelper {
     static syntaxSpecifics = {
         separator: "::",
-        commandCodes: {
+        sendingCommandCodes: {
             manualLogin: "101",
             autoReconnect: "103",
             logOut: "105"
+        },
+        receivingCommandCodes: {
+            "101": "102",
+            "103": "104",
+        
         }
     }
 
-
     static buildLoginString = (username, password) => {
-        return this.syntaxSpecifics.commandCodes.manualLogin + this.syntaxSpecifics.separator +
+        return this.syntaxSpecifics.sendingCommandCodes.manualLogin + this.syntaxSpecifics.separator +
         username + this.syntaxSpecifics.separator +
         password;
     }
 
     static buildLogoutString = () => {
-        return this.syntaxSpecifics.commandCodes.logOut;
+        return this.syntaxSpecifics.sendingCommandCodes.logOut;
     }
 
     static buildAutoLoginString = (username, token) => {
-        return this.syntaxSpecifics.commandCodes.autoReconnect + this.syntaxSpecifics.separator +
+        return this.syntaxSpecifics.sendingCommandCodes.autoReconnect + this.syntaxSpecifics.separator +
         username + this.syntaxSpecifics.separator +
         token;
     }
+    
+
 }
  
 export default HoSoHelper;
