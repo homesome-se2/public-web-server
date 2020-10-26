cd62be12799caf88aa76e0e81aa9fdb3fc846c35
Vincenzo <vincebshock@gmail.com>
2020-10-26 19:26:03 +0100

    auto-login data logger added

--- src/pages/dashboard/DashboardPage.js
+++ src/pages/dashboard/DashboardPage.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 import { UserContext } from "../../contexts/UserContext";
+import HoSoHelper from "../../helpers/HoSoHelper";
+import LSTokenService from "../../services/LSTokenService";
 
 class DashboardPage extends Component {
   static contextType = UserContext;
 
   constructor(props) {
     super(props);
-    this.state = {
-      
-    };
-
+    this.state = {};
   }
   componentDidMount() {
-    this.context.csInstance.listenForUpdates();
+    console.log(HoSoHelper.buildAutoLoginString('213213', 'DAB8D24756D3F0EB2F3A032477A3CFF3'));
+    this.context.csInstance.connect();
+    this.context.csInstance
+      .autoLogin(LSTokenService.getUsername(), LSTokenService.getToken())
+      .then((rData) => {
+        console.log("autologin data: ", rData);
+      });
   }
 
   render() {
