e68802483bd6deb03cfbb61c1192c8c2b1dcaa15
Vincenzo <vincebshock@gmail.com>
2020-10-27 08:54:23 +0100

    💄✨ pane-active-area basic implementation & styling added

--- src/components/pane/pane-active-area/PaneActiveArea.js
+++ src/components/pane/pane-active-area/PaneActiveArea.js
@@ -8,6 +8,14 @@ class PaneActiveArea extends Component {
             <div className="pane pane-active-area">
                 <div className="inner-area">
                     <div className="content-wrapper">
+                        <div className="no-scroll-area device-list-header-wrapper">
+                            <span className="device-list-header-group">
+                                <h2 className="medium-title d1-title">Device list</h2>
+                                <h2 className="medium-subtitle d2-subtitle">All the available gadgets</h2>
+                            </span>
+                            <div className="separator"></div>
+
+                        </div>
                         
                     </div>
                 </div>
