4c2f231e5f838d87d5a4007fc6d1a4de93b5f64b
Vincenzo <vincebshock@gmail.com>
2020-10-27 05:28:33 +0100

    wsReceiverEEmitter at user-context level with subscribers added

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,41 +1,117 @@
-import React, {Component, createContext} from 'react';
-import ConnectionService from '../services/ConnectionService';
+import React, { Component, createContext } from "react";
+import ConnectionService from "../services/ConnectionService";
 
 export const UserContext = createContext();
 
 class UserContextProvider extends Component {
-    state = { 
-        username: '',
-        isAdmin: '',
-        homeAlias: '',
-        token: '',
-        gadgets: [],
-        csInstance: new ConnectionService('ws://134.209.198.123:8084/homesome')
-     }
-     updateGadgets = (gadgets) => {
-        this.setState({gadgets: gadgets})
-     }
-     setUsername = (username) => {
-        this.setState({username: username})
-     }
-     setAdminFlag = (adminFlag) => {
-        this.setState({isAdmin: adminFlag})
-     }
-     setHomeAlias = (homeAlias) => {
-        this.setState({homeAlias: homeAlias})
-     }
-     setToken = (token) => {
-        this.setState({token: token})
-     }
-   
-    render() { 
-        return ( 
-            <UserContext.Provider value={ {...this.state, updateGadgets: this.updateGadgets, setUsername: this.setUsername, 
-            setAdminFlag: this.setAdminFlag, setHomeAlias: this.setHomeAlias, setToken: this.setToken} }>
-                {this.props.children}
-            </UserContext.Provider>
-         );
-    }
+  state = {
+    username: "",
+    isAdmin: "",
+    homeAlias: "",
+    token: "",
+    gadgets: [],
+    csInstance: new ConnectionService("ws://134.209.198.123:8084/homesome"),
+    subscribers: {
+      connectionOpenRV: null,
+      gadgetFetchRV: null,
+      gadgetStateUpdate: null,
+    },
+  };
+  updateGadgets = (gadgets) => {
+    this.setState({ gadgets: gadgets });
+  };
+  setUsername = (username) => {
+    this.setState({ username: username });
+  };
+  setAdminFlag = (adminFlag) => {
+    this.setState({ isAdmin: adminFlag });
+  };
+  setHomeAlias = (homeAlias) => {
+    this.setState({ homeAlias: homeAlias });
+  };
+  setToken = (token) => {
+    this.setState({ token: token });
+  };
+  setupEESubscribers = () => {
+    console.log(this.state.csInstance.getWSReceiverEEmitterInstance);
+    this.setState({
+      subscribers: {
+        gadgetStateUpdate: this.state.csInstance
+          .getWSReceiverEEmitterInstance()
+          .subscribe(
+            ConnectionService.wsReceiverEEmitterEvent.onGadgetStateUpdateRV,
+            (...args) => {
+              console.log(args);
+            }
+          ),
+      },
+    });
+    this.setState({
+      subscribers: {
+         connectionOpenRV: this.state.csInstance
+          .getWSReceiverEEmitterInstance()
+          .subscribe(
+            ConnectionService.wsReceiverEEmitterEvent.onConnectionOpenRV,
+            (...args) => {
+              console.log(args);
+            }
+          ),
+      },
+    });
+    this.setState({
+      subscribers: {
+         connectionOpenRV: this.state.csInstance
+          .getWSReceiverEEmitterInstance()
+          .subscribe(
+            ConnectionService.wsReceiverEEmitterEvent.onGadgetFecthRV,
+            (...args) => {
+              console.log(args);
+            }
+          ),
+      },
+    });
+  };
+
+  csConnect = () => {
+    this.setupEESubscribers();
+    return this.state.csInstance.connect();
+  };
+  login = (username, password) => {
+    return new Promise((resolve, reject) => {
+      this.state.csInstance
+        .auth(ConnectionService.authTypes.manualLogin, username, password)
+        .then((rData) => {
+          //you're auth with no errors!
+          this.initLiveHooks();
+          resolve(rData);
+        })
+        .catch((rData) => {
+          reject(rData);
+        });
+    });
+  };
+  initLiveHooks = () => {
+   this.state.csInstance.initLiveHooks();
+  };
+
+  render() {
+    return (
+      <UserContext.Provider
+        value={{
+          ...this.state,
+          updateGadgets: this.updateGadgets,
+          setUsername: this.setUsername,
+          setAdminFlag: this.setAdminFlag,
+          setHomeAlias: this.setHomeAlias,
+          setToken: this.setToken,
+          csConnect: this.csConnect,
+          login: this.login,
+        }}
+      >
+        {this.props.children}
+      </UserContext.Provider>
+    );
+  }
 }
- 
+
 export default UserContextProvider;
