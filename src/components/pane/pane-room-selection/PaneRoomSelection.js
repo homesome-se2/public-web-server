56b087cd4abcac74b2a96c0048863745ea5d535e
Vincenzo <vincebshock@gmail.com>
2020-10-27 08:15:07 +0100

    💄✨ pane-room-selection basic implementation & styling added

--- src/components/pane/pane-room-selection/PaneRoomSelection.js
+++ src/components/pane/pane-room-selection/PaneRoomSelection.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import "./PaneRoomSelection.css";
 
 class PaneRoomSelection extends Component {
     state = {  }
     render() { 
-        return ( null );
+        return ( 
+            <div className="pane pane-room-selection">
+                <div className="content-wrapper">
+                    <h1 className="medium-title d1-title">Rooms</h1>
+                    <h2 className="medium-subtitle d2-subtitle">Select the room you want filter the gadgets by</h2>
+                </div>
+            </div>
+         );
     }
 }
  
-export default PaneRoomSelectinullon;
+export default PaneRoomSelection;
