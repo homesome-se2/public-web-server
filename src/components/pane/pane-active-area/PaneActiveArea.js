80b1ddfb2f9278ccfb56d60651cb852b883262ad
Vincenzo <vincebshock@gmail.com>
2020-10-27 08:16:28 +0100

    💄✨ pane-active-area basic implementation & styling added

--- src/components/pane/pane-active-area/PaneActiveArea.js
+++ src/components/pane/pane-active-area/PaneActiveArea.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import './PaneActiveArea.css';
 
 class PaneActiveArea extends Component {
     state = {  }
     render() { 
-        return ( null );
+        return ( 
+            <div className="pane pane-active-area">
+                <div className="inner-area">
+                    <div className="content-wrapper">
+                        
+                    </div>
+                </div>
+            </div>
+         );
     }
 }
  
