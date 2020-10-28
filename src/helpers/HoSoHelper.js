bcbfa010b87d6eafccfa25209d70fa8bea1ae688
Vincenzo <vincebshock@gmail.com>
2020-10-28 11:22:10 +0100

    ✨ HoSo Helper: error exception handled (workaround)

--- src/helpers/HoSoHelper.js
+++ src/helpers/HoSoHelper.js
@@ -2,19 +2,22 @@ import Gadget from "../models/Gadget";
 
 class HoSoHelper {
   static syntaxSpecifics = {
-    separator: '::',
+    separator: "::",
     sendingCommandCodes: {
-      manualLogin: '101',
-      autoReconnect: '103',
-      logOut: '105',
+      manualLogin: "101",
+      autoReconnect: "103",
+      logOut: "105",
     },
     receivingCommandCodes: {
-      sucessfulManualLogin: '102',
-      successfulAutoLogin: '104',
-      gadgetFetching: '304',
-      gadgetStateUpdate: '316',
-      error: '901',
+      sucessfulManualLogin: "102",
+      successfulAutoLogin: "104",
+      gadgetFetching: "304",
+      gadgetStateUpdate: "316",
+      serverException: "901",
     },
+    invalidLocalCodes: {
+      invalid: '_INVALID_'
+    }
   };
 
   static buildLoginString = (username, password) => {
@@ -51,17 +54,20 @@ class HoSoHelper {
   };
   static buildGadgetObjectArray = (paramsArray) => {
     let gadgetList = [];
-    for(let i=0; i<Number(paramsArray[0]); i++){
-      gadgetList.push(new Gadget(
-        paramsArray[i*Gadget.gadgetProperties+1], 
-        paramsArray[i*Gadget.gadgetProperties+2], 
-        paramsArray[i*Gadget.gadgetProperties+3],
-        paramsArray[i*Gadget.gadgetProperties+4], 
-        paramsArray[i*Gadget.gadgetProperties+5], 
-        paramsArray[i*Gadget.gadgetProperties+6]))
+    for (let i = 0; i < Number(paramsArray[0]); i++) {
+      gadgetList.push(
+        new Gadget(
+          paramsArray[i * Gadget.gadgetProperties + 1],
+          paramsArray[i * Gadget.gadgetProperties + 2],
+          paramsArray[i * Gadget.gadgetProperties + 3],
+          paramsArray[i * Gadget.gadgetProperties + 4],
+          paramsArray[i * Gadget.gadgetProperties + 5],
+          paramsArray[i * Gadget.gadgetProperties + 6]
+        )
+      );
     }
     return gadgetList;
-  }
+  };
   static parseString = (string) => {
     let params = string.split("::");
     return {
@@ -69,52 +75,70 @@ class HoSoHelper {
       params: [...params.slice(1)],
     };
   };
-  static parseHoSoMessage = message => {
-      switch(this.parseString(message).commandCode){
-            case HoSoHelper.syntaxSpecifics.receivingCommandCodes["successfulAutoLogin"]:
-                //TODO: REQUEST SUCESSAUTOLOGIN RESPONSE PARAM UPDATE
-               //polyfill empty values
-                return {
-                    type: 'AUTO_LOGIN_RESULT',
-                    result: 'SUCCESS',
-                    username: '',
-                    isAdmin: '', 
-                    homeAlias: '',
-                    token: ''
-                }
-            case HoSoHelper.syntaxSpecifics.receivingCommandCodes["sucessfulManualLogin"]:
-               return {
-                    type: 'MANUAL_LOGIN_RESULT',
-                    result: 'SUCCESS',
-                    username: this.parseString(message).params[0],
-                    isAdmin: this.parseString(message).params[1],
-                    homeAlias: this.parseString(message).params[2],
-                    token: this.parseString(message).params[3]
-                }
-            case HoSoHelper.syntaxSpecifics.receivingCommandCodes["unsuccessfulLogin"]:
-               return {
-                    type: 'ERROR',
-                    errorCode: HoSoHelper.syntaxSpecifics.receivingCommandCodes["unsuccessfulLogin"],
-                    description: this.parseString(message).params[0],
-                }
-            case HoSoHelper.syntaxSpecifics.receivingCommandCodes["gadgetFetching"]:
-               return {
-                    type: 'GADGET_LIST',
-                    gadgets: this.buildGadgetObjectArray(this.parseString(message).params)
-                }
-              case HoSoHelper.syntaxSpecifics.receivingCommandCodes["gadgetStateUpdate"]:
-               return {
-                    type: 'GADGET_STATE_UPDATE',
-                    gadgetId: this.parseString(message).params[0],
-                    updatedValue: this.parseString(message).params[1],
-                }
-            default: 
-            return {
-                type: 'ERROR',
-                description: 'invalid message format',
-            }
-      }
-  }
+  static parseHoSoMessage = (message) => {
+    switch (this.parseString(message).commandCode) {
+      case HoSoHelper.syntaxSpecifics.receivingCommandCodes[
+        "successfulAutoLogin"
+      ]:
+        //TODO: REQUEST SUCESSAUTOLOGIN RESPONSE PARAM UPDATE
+        //polyfill empty values
+        return {
+          type: "AUTO_LOGIN_RESULT",
+          result: "SUCCESS",
+          username: "",
+          isAdmin: "",
+          homeAlias: "",
+          token: "",
+        };
+      case HoSoHelper.syntaxSpecifics.receivingCommandCodes[
+        "sucessfulManualLogin"
+      ]:
+        return {
+          type: "MANUAL_LOGIN_RESULT",
+          result: "SUCCESS",
+          username: this.parseString(message).params[0],
+          isAdmin: this.parseString(message).params[1],
+          homeAlias: this.parseString(message).params[2],
+          token: this.parseString(message).params[3],
+        };
+      case HoSoHelper.syntaxSpecifics.receivingCommandCodes[
+        "unsuccessfulLogin"
+      ]:
+        return {
+          type: "ERROR",
+          errorCode:
+            HoSoHelper.syntaxSpecifics.receivingCommandCodes[
+              "unsuccessfulLogin"
+            ],
+          description: this.parseString(message).params[0],
+        };
+      case HoSoHelper.syntaxSpecifics.receivingCommandCodes["gadgetFetching"]:
+        return {
+          type: "GADGET_LIST",
+          gadgets: this.buildGadgetObjectArray(
+            this.parseString(message).params
+          ),
+        };
+      case HoSoHelper.syntaxSpecifics.receivingCommandCodes[
+        "gadgetStateUpdate"
+      ]:
+        return {
+          type: "GADGET_STATE_UPDATE",
+          gadgetId: this.parseString(message).params[0],
+          updatedValue: this.parseString(message).params[1],
+        };
+      case HoSoHelper.syntaxSpecifics.receivingCommandCodes["serverException"]:
+        return {
+          type: "SERVER_EXCEPTION",
+          description: this.parseString(message).params[0],
+        };
+      default:
+        return {
+          type: "ERROR",
+          description: "invalid or unhandled response message format",
+        };
+    }
+  };
 }
 
 export default HoSoHelper;
