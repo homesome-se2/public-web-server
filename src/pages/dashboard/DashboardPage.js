b7f1ff4e717a10b737313b5136dc29096cdcb5ca
hamid <alijani.hamid@yahoo.com>
2020-10-30 01:19:08 +0100

    PaneActiveArea on DashboardPage styled.

--- src/pages/dashboard/DashboardPage.js
+++ src/pages/dashboard/DashboardPage.js
@@ -29,7 +29,9 @@ class DashboardPage extends Component {
             <PaneRoomSelection />
           </Col>
           <Col xs={24} sm={24} lg={14}>
-           <PaneActiveArea/>
+            <div style={{backgroundColor:'#f3f6ff',width:'100%',height:'85%'}}>
+           <PaneActiveArea />
+            </div>
           </Col>
           <Col xs={24} sm={24} lg={6}>
             <PaneDetails />
