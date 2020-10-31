da05b6e6ca94d83d1e2be04621e78c75115a87d3
hamid <alijani.hamid@yahoo.com>
2020-10-31 23:07:57 +0100

    The "PaneActiveArea import" moved up below the PaneRoomSelection on the DashboardPage

--- src/pages/dashboard/DashboardPage.js
+++ src/pages/dashboard/DashboardPage.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 import { Row, Col } from "antd"
 import PaneDetails from '../../components/pane/panedetails/PaneDetails'
 import PaneRoomSelection from '../../components/pane/paneroomselection/PaneRoomSelection';
+import PaneActiveArea from '../../components/pane/paneactivearea/PaneActiveArea';
 import { UserContext } from "../../contexts/UserContext";
 import 'antd/dist/antd.css';
 import './DashboardPage.css';
-import PaneActiveArea from '../../components/pane/paneactivearea/PaneActiveArea';
 
 
 
@@ -30,7 +30,7 @@ class DashboardPage extends Component {
           </Col>
           <Col xs={24} sm={24} lg={14}>
             <div style={{backgroundColor:'#f3f6ff',width:'100%',height:'85%'}}>
-           <PaneActiveArea />
+           <PaneActiveArea/>
             </div>
           </Col>
           <Col xs={24} sm={24} lg={6}>
