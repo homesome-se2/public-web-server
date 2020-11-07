1b6ec9411c41e52e74ef42d2f7e20df5db3f059b
Vincenzo <vincebshock@gmail.com>
2020-11-07 08:12:06 +0100

    🎨✨ Full HoSo specifics added

--- src/HoSoProtocolStack/HoSoSpecifics.js
+++ src/HoSoProtocolStack/HoSoSpecifics.js
@@ -1,24 +1,62 @@
 class HoSoSpecifics {
-    static syntaxSpecifics = {
-        separator: '::',
-        sendingCommandCodes: {
-          manualLogin: '101',
-          autoReconnect: '103',
-          logOut: '105',
-        },
-        receivingCommandCodes: {
-          successfulManualLogin: '102',
-          successfulAutoLogin: '104',
-          gadgetFetching: '304',
-          gadgetStateUpdate: '316',
-          serverException: '901',
-          unsuccessfulLogin: '903'
-          
-        },
-        invalidLocalCodes: {
-          invalid: '_INVALID_'
-        }
-      };
+  static syntax = {
+    singleSeparator: ':',
+    doubleSeparator: '::',
+  };
+  static commandCodes = {
+    sending: {
+      auth: {
+        manualLogin: '101',
+        autoLogin: '103',
+        logOut: '105',
+      },
+      gadgetState: {
+        fetchGadgets: '301',
+        fetchGadgetGroups: '370',
+        alterGadgetState: '311',
+      },
+    },
+    receiving: {
+      auth: {
+        successfulManualLogin: '102',
+        successfulAutoLogin: '104',
+        unsuccessfulLogin: '903',
+      },
+      gadgetState: {
+        fetchGadgets: '304',
+        fetchGadgetGroups: '373',
+        gadgetStateChange: '316',
+        GadgetAddition: '352',
+        gadgetRemoval: '354',
+      },
+      global: {
+        exception: '901',
+        ping: 'ping',
+      },
+    },
+  };
+
+  static syntaxSpecifics = {
+    separator: '::',
+    sendingCommandCodes: {
+      auth: {
+        manualLogin: '101',
+        autoLogin: '103',
+        logOut: '105',
+      },
+    },
+    receivingCommandCodes: {
+      successfulManualLogin: '102',
+      successfulAutoLogin: '104',
+      gadgetFetching: '304',
+      gadgetStateUpdate: '316',
+      serverException: '901',
+      unsuccessfulLogin: '903',
+    },
+    invalidLocalCodes: {
+      invalid: '_INVALID_',
+    },
+  };
 }
- 
+
 export default HoSoSpecifics;
