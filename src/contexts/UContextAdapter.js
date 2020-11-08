e10ac84ffdf115bf7c2184cdab1ee0a57efa2616
Vincenzo <vincebshock@gmail.com>
2020-11-08 17:09:53 +0100

    ✨UContextAdapter updateUCUserData, updateUCGadgetData, BuildGObjectArray

--- src/contexts/UContextAdapter.js
+++ src/contexts/UContextAdapter.js
@@ -1,14 +1,36 @@
+import Gadget from '../models/Gadget';
+
 class UContextAdapter {
   constructor() {
     this.CChangeListener();
   }
   CChangeListener = () => {};
-  static updateUCUserData = (state, update) => {
-    state.username = update.props.C_nameID;
-    state.isAdmin = update.props.C_isAdmin;
-    state.token = update.props.C_SessionKey;
-    state.homeAlias = update.props.H_Alias;
+  static updateUCUserData = (state, ...update) => {
+    state.username = update[0].props.C_nameID;
+    state.isAdmin = update[0].props.C_isAdmin;
+    state.token = update[0].props.C_SessionKey;
+    state.homeAlias = update[0].props.H_Alias;
     return state;
+  };
+  static updateUCGadgetData = (state, ...update) => {
+    state.gadgets = update[0].props?.gadgets;
+    return state;
+  };
+  static buildGadgetObjectArray = (paramsArray) => {
+    let gadgetList = [];
+    for (let i = 0; i < Number(paramsArray[0]); i++) {
+      gadgetList.push(
+        new Gadget(
+          paramsArray[i * Gadget.gadgetProperties + 1],
+          paramsArray[i * Gadget.gadgetProperties + 2],
+          paramsArray[i * Gadget.gadgetProperties + 3],
+          paramsArray[i * Gadget.gadgetProperties + 4],
+          paramsArray[i * Gadget.gadgetProperties + 5],
+          paramsArray[i * Gadget.gadgetProperties + 6]
+        )
+      );
+    }
+    return gadgetList;
   };
 }
 
