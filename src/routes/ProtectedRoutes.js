e55782b27a54bc89a8bcefbd20f60b17453b7839
Vincenzo <vincebshock@gmail.com>
2020-11-10 04:06:41 +0100

    ✨ProtectedRoute: updated isAuth with CryptoGuard HS256

--- src/routes/ProtectedRoutes.js
+++ src/routes/ProtectedRoutes.js
@@ -1,23 +1,32 @@
-import React, {Component} from 'react';
-import { Redirect } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import AuthCryptoGuard from '../helpers/AuthCryptoGuard';
 import LSTokenService from '../services/LSTokenService';
 
-
-
 class ProtectedRoutes extends Component {
-    state = {  }
-    render() { 
-        const Component = this.props.component;
-        const isAuthenticated = LSTokenService.isAuth();
+  state = {};
+  render() {
+    const Component = this.props.component;
+    const isAuthenticated =
+      LSTokenService.isAuth() &&
+      AuthCryptoGuard.verify(
+        AuthCryptoGuard.getBase64Encoding(
+          AuthCryptoGuard.generateHash(
+            LSTokenService.getToken(),
+            LSTokenService.getUsername()
+          )
+        ),
+        LSTokenService.getHash()
+      );
 
-        //TODO: TOKEN VERIFICATION
-       
-        return isAuthenticated ? (
-            <Component />
-        ) : (
-            <Redirect to={{ pathname: '/login' }} />
-        );
-    }
+    //TODO: TOKEN VERIFICATION
+
+    return isAuthenticated ? (
+      <Component />
+    ) : (
+      <Redirect to={{ pathname: '/login' }} />
+    );
+  }
 }
- 
+
 export default ProtectedRoutes;
