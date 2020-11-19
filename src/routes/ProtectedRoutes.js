6fa86032434ed7b1279aa426dd32ab2bd7750c1b
Vincenzo <vincebshock@gmail.com>
2020-11-19 15:41:39 +0100

    ⚡️🎨✨protected-routes: auth-reconnect integration

--- src/routes/ProtectedRoutes.js
+++ src/routes/ProtectedRoutes.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import { UserContext } from '../contexts/UserContext';
+import stEEmitterRVHub from '../EEmitters/RVHubs/stEEmitterRVHub';
 import LSTokenService from '../services/LSTokenService';
 
 class ProtectedRoutes extends Component {
+  static contextType = UserContext;
+
   state = {};
+
+  componentDidMount() {
+    this.context.lifecycleHooks
+      .getEEInstance()
+      .subscribe(stEEmitterRVHub.events.onStateMounted, this.stateDidMount);
+  }
+
+  stateDidMount = () => {};
+
+  isAuthorized = () => {
+    if (this.context != null && this.context.isAuth) return 1;
+    if (LSTokenService.isEligibleForAutoAuth()) return 0;
+    return -1;
+  };
+
   render() {
     const Component = this.props.component;
-    const isAuthenticated =
-      LSTokenService.isAuth() && LSTokenService.isEligibleForAutoAuth();
+    const isAuthenticated = this.isAuthorized();
 
-    //TODO: TOKEN VERIFICATION
+    if (isAuthenticated === 1) return <Component />;
+    if (isAuthenticated === 0)
+      return <Redirect to={{ pathname: '/reconnect' }} />;
 
-    return isAuthenticated ? (
-      <Component />
-    ) : (
-      <Redirect to={{ pathname: '/login' }} />
-    );
+    return <Redirect to={{ pathname: '/login' }} />;
   }
 }
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,21 @@
 import LoginPage from '../pages/auth/login/LoginPage';
+import Reconnect from '../pages/auth/reconnect/Reconnect';
 import DashBoardPage from '../pages/dashboard/DashboardPage';
 
 const routes = [
-    {
-        path: "/dashboard",
-        component: DashBoardPage,
-        title: "Dashboard"
-    },
-    {
-        path: "/auth/login",
-        component: LoginPage,
-        title: "Sign in"
-    }
-]
+  {
+    path: '/dashboard',
+    component: DashBoardPage,
+    title: 'Dashboard',
+  },
+  {
+    path: '/auth/login',
+    component: LoginPage,
+    title: 'Sign in',
+  },
+  {
+    path: '/auth/reconnect',
+    component: Reconnect,
+    title: 'Reconnect',
+  },
+];
