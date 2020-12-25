ad9fca47b9bfd9dea292b193f9d05b554b1b3f2c
Vincenzo <vincebshock@gmail.com>
2020-12-25 17:06:20 +0100

    ✨home-alias and search-bar added

--- src/components/pane/pane-active-area/PaneActiveArea.js
+++ src/components/pane/pane-active-area/PaneActiveArea.js
@@ -9,6 +9,10 @@ import ucEEmitterRVHub from '../../../EEmitters/RVHubs/ucEEmitterRVHub';
 class PaneActiveArea extends Component {
   static contextType = UserContext;
 
+  state = {
+    searchText: '',
+  };
+
   componentDidMount() {
     setTimeout(() => {
       this.setupEESubscribers();
@@ -35,14 +39,33 @@ class PaneActiveArea extends Component {
         }
       );
   };
-  state = {};
+  searchText = (e) => {
+    this.setState({ searchText: e.target.value });
+  };
   render() {
     return (
       <div className="pane-active-area">
         <div className="inset-area">
           <div className="content-wrapper">
-            <h1 className="header black-medium">Gadget list</h1>
-            <h2 className="sub-header light-grey">All the available gadget</h2>
+            <div className="left-wrapper">
+              <h1 className="header black-medium">Gadget list</h1>
+              <h2 className="sub-header light-grey">
+                All the available gadget
+              </h2>
+            </div>
+            <div className="right-wrapper">
+              <h1 className="home-alias">
+                Home alias: <span>{this.context.homeAlias}</span>
+              </h1>
+              <input
+                className="search-bar"
+                type="text"
+                placeholder="Type to search..."
+                onChange={(e) => {
+                  this.searchText(e);
+                }}
+              ></input>
+            </div>
             <div className="divider"></div>
             <Grid
               container
@@ -52,12 +75,14 @@ class PaneActiveArea extends Component {
             >
               {this.context.gadgets.map((gadget, key) => {
                 if (
-                  1 || //=REMOVE-ME, NOTIFY ANTON BUG
+                  //1 || //=REMOVE-ME, NOTIFY ANTON BUG
                   (this.context != null &&
                     this.context.selectedGadgetGroup != null &&
                     this.context.selectedGadgetGroup.gadgetIds.includes(
                       gadget.id
-                    ))
+                    ) &&
+                    this.state.searchText.length > 0) ||
+                  gadget.alias.includes(this.state.searchText)
                 )
                   return <GadgetCard gadget={gadget} key={key} />;
 
