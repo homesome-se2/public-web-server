d02849142c469ec3e352a4fbcaafb6edf542a918
Vincenzo <vincebshock@gmail.com>
2020-12-30 06:17:37 +0100

    ✨gadget-control-expanded-setValue added

---
+++ src/components/gadget/gadget-card/gadget-control-expanded-setValue/GadgetControlExpandedSetValue.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { UserContext } from '../../../../contexts/UserContext';
+import './GadgetControlExpandedSetValue.css';
+
+class GadgetControlExpandedSetValue extends Component {
+  static contextType = UserContext;
+
+  state = {
+    percentage: this.context.selectedGadget.state,
+  };
+
+  handleChange = (e) => {
+    this.setState({ percentage: e.target.value });
+    //this.props.onValueChange(e.target.value);
+  };
+  render() {
+    return (
+      <div className="gadget-control-expanded-setValue">
+        <div className="wrapper">
+          <h2 className="sub-header light-grey value-sub-header">Value</h2>
+          <div className={`percentage`}>
+            {(Math.round(this.state.percentage * 10) / 10).toFixed(1)}
+          </div>
+          <div className="slider-wrapper">
+            <input
+              type="range"
+              min="0"
+              max="100"
+              step="1.0"
+              value={this.state.percentage}
+              className="v-slider"
+              onChange={(e) => {
+                this.handleChange(e);
+              }}
+              onBlur={(e) => {
+                //this.props.onStateAltered(e);
+              }}
+            ></input>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GadgetControlExpandedSetValue;
